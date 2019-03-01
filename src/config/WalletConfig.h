79b375da3033b758c6fc8a71f40301ca039ae980
novay246 <36161049+novay246@users.noreply.github.com>
2019-02-28 23:47:58 -0500

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "OSC";
+    const std::string_view addressPrefix = "hann";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "OSC";
