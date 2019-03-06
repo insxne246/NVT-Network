b003a4f619fd66d5d49739c388b7456ae6f3b705
oscillate-coin <36161049+oscillate-coin@users.noreply.github.com>
2019-03-06 09:26:12 -0500

    Update Ascii.h
    Ascii Art fix

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, The TurtleCoin Developers
 //
 // Please see the included LICENSE file for more information
-
+//Thanks to  arifyan for fixing it
 #pragma once
 
 const std::string windowsAsciiArt =
@@ -11,16 +11,15 @@ const std::string windowsAsciiArt =
      "| | | |/ __| / __|| || || | / _` || __| / _ \\n"
      "\\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
       "\\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
-                                             
-                                             
 
-const std::string nonWindowsAsciiArt = 
-     "\n_____             _  _  _         _ \n"        
-     "|  _  |           (_)| || |       | |\n"        
+
+const std::string nonWindowsAsciiArt =
+     "\n _____             _  _  _         _ \n"
+     "|  _  |           (_)| || |       | |\n"
      "| | | | ___   ___  _ | || |  __ _ | |_   ___ \n"
-     "| | | |/ __| / __|| || || | / _` || __| / _ \\n"
+     "| | | |/ __| / __|| || || | / _` || __| / _ \\\n"
      "\\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
-      "\\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
+     " \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
    the art itself, and remove these two #ifdefs and above ascii arts */
