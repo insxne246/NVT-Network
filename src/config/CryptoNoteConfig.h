5f9cd5ac7837bd4c1778750d9de6d2fb9760dbee
pxckets <49133592+pxckets@users.noreply.github.com>
2019-05-22 18:21:20 -0400

    Update CryptoNoteConfig.h
    derp

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -167,7 +167,7 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    187000 //Undetermined yet
+    120000 //MIXIN upgrade to prevent attack
  
 };
 
